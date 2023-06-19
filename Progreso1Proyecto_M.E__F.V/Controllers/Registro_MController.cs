2a0649c939310ce8a22072b3365c8afb77b592c9
CamilaCabrera <camilacabrera464@gmail.com>
2023-06-18 23:05:31 -0500

    subir

--- Progreso1Proyecto_M.E__F.V/Controllers/Registro_MController.cs
+++ Progreso1Proyecto_M.E__F.V/Controllers/Registro_MController.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Progreso1Proyecto_M.E__F.V.Data;
 using Progreso1Proyecto_M.E__F.V.Models;
 using Microsoft.AspNetCore.Authorization;
-
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Progreso1Proyecto_M.E__F.V.Controllers
 {
@@ -16,13 +16,34 @@ namespace Progreso1Proyecto_M.E__F.V.Controllers
     {
         private readonly Progreso1Proyecto_ME__FVContext _context;
 
+        Log logger = Log.Instance;
+
         public Registro_MController(Progreso1Proyecto_ME__FVContext context)
         {
             _context = context;
         }
 
-        // GET: Registro_M
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Cerrar el archivo de registro
+                logger.CloseLogFile();
+            }
+            base.Dispose(disposing);
+        }
 
+        public class LogActionFilter : ActionFilterAttribute
+        {
+            public override void OnActionExecuted(ActionExecutedContext context)
+            {
+                Log logger = Log.Instance;
+                logger.CloseLogFile();
+                base.OnActionExecuted(context);
+            }
+        }
+
+        // GET: Registro_M
         public async Task<IActionResult> Index(string buscar, int filtro)
         {
             var registro_M = from Registro_M in _context.Registro_M select Registro_M;
@@ -68,17 +89,17 @@ namespace Progreso1Proyecto_M.E__F.V.Controllers
         // GET: Registro_M/Create
         public IActionResult Create()
         {
+            logger.LogButtonClick("Crear");
             return View();
         }
 
         // POST: Registro_M/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Semestre,Materia,Profesor,Calificacion,Descripcion,Cualidad,Horario")] Registro_M registro_M)
         {
+            logger.LogButtonClick("Registrar");
             if (ModelState.IsValid)
             {
                 _context.Add(registro_M);
@@ -92,6 +113,7 @@ namespace Progreso1Proyecto_M.E__F.V.Controllers
         [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
+            logger.LogButtonClick("Editar");
             if (id == null || _context.Registro_M == null)
             {
                 return NotFound();
@@ -106,13 +128,12 @@ namespace Progreso1Proyecto_M.E__F.V.Controllers
         }
 
         // POST: Registro_M/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Semestre,Materia,Profesor,Calificacion,Descripcion,Cualidad,Horario")] Registro_M registro_M)
         {
+            logger.LogButtonClick("Editar");
             if (id != registro_M.Id)
             {
                 return NotFound();
@@ -140,10 +161,12 @@ namespace Progreso1Proyecto_M.E__F.V.Controllers
             }
             return View(registro_M);
         }
+
         [Authorize]
         // GET: Registro_M/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
+            logger.LogButtonClick("Borrar");
             if (id == null || _context.Registro_M == null)
             {
                 return NotFound();
@@ -165,9 +188,10 @@ namespace Progreso1Proyecto_M.E__F.V.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
+            logger.LogButtonClick("Borrar");
             if (_context.Registro_M == null)
             {
-                return Problem("Entity set 'Progreso1Proyecto_ME__FVContext.Registro_M'  is null.");
+                return Problem("Entity set 'Progreso1Proyecto_ME__FVContext.Registro_M' is null.");
             }
             var registro_M = await _context.Registro_M.FindAsync(id);
             if (registro_M != null)
@@ -183,6 +207,5 @@ namespace Progreso1Proyecto_M.E__F.V.Controllers
         {
             return (_context.Registro_M?.Any(e => e.Id == id)).GetValueOrDefault();
         }
-
     }
 }
---
+++ Progreso1Proyecto_M.E__F.V/Models/Log.cs
@@ -0,0 +1,70 @@
+﻿using System.Net;
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Progreso1Proyecto_M.E__F.V.Models
+{
+    public sealed class Log
+    {
+        private static Log instance = null;
+        private static readonly object lockObject = new object();
+        private StreamWriter logWriter;
+        private string ipAddress;
+
+        private Log()
+        {
+            logWriter = new StreamWriter("log.txt", true);
+            ipAddress = GetLocalIPAddress();
+        }
+
+        public static Log Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Log();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public void LogButtonClick(string action)
+        {
+            string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            string logEntry = $"{timestamp}|{ipAddress}|{action}";
+            logWriter.WriteLine(logEntry);
+        }
+
+        public void CloseLogFile()
+        {
+            logWriter.Close();
+            instance = null; // Reset the instance for reinitialization
+        }
+
+        private string GetLocalIPAddress()
+        {
+            string ipAddress = string.Empty;
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    ipAddress = ip.ToString();
+                    break;
+                }
+            }
+
+            return ipAddress;
+        }
+    }
+}
+
